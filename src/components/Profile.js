6dfd699b9c5e710395d4645381ffe7afab3070d6
vogtn <nicolasjvogt@gmail.com>
2017-04-18 17:46:01 -0700

    more profiles

---
+++ src/components/Profile.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
+import ProfilePosts from '../components/ProfilePosts'
+
+
+class Profile extends React.Component {
+render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+    return (
+        <div>
+            <p>name: {this.props.data.user.name}</p>
+            <p>email: {this.props.data.user.emailAddress}</p>
+            <p>Credit Card #: {this.props.data.user.creditCard}</p>
+            <p>point: {this.props.data.user.point}</p>
+            <p>steak: {this.props.data.user.streak}</p>
+        </div>
+    )
+  }
+}
+
+const userQuery = gql`
+  query userQuery {
+    user {
+      id
+      name
+      emailAddress
+      creditCard
+      point
+      streak
+      image
+    }
+  }
+`
+
+export default graphql(userQuery)(Profile)
---
+++ src/components/ProfilePosts.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
+import Post from '../components/Post'
+
+
+class ProfilePosts extends React.Component {
+render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+    return (
+     <div className='w-100 flex justify-center'>
+     {console.log(this.props.data)}
+        <div className='w-100' style={{ maxWidth: 400 }}>
+          {this.props.data.allPosts.map((post) =>
+            <Post key={post.id} post={post} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const FeedQuery = gql`
+query FeedQuery {
+  allPosts(orderBy: id_DESC) {
+    id
+    imageUrl
+    title
+    description
+    pointCost
+    start
+    finish
+  }
+}`
+
+export default graphql(FeedQuery)(ProfilePosts)
